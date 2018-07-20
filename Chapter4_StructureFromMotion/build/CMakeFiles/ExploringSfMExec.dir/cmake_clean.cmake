2d14cae9be0e67203135188470685a9687356676
feifanrensheng <feifanrensheng1@outlook.com>
2018-07-20 13:49:10 +0800

    add files

---
+++ Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMExec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExploringSfMExec.dir/Visualization.cpp.o"
+  "CMakeFiles/ExploringSfMExec.dir/main.cpp.o"
+  "ExploringSfMExec.pdb"
+  "ExploringSfMExec"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ExploringSfMExec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/DependInfo.cmake
@@ -0,0 +1,72 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zn/Chapter4_StructureFromMotion/AbstractFeatureMatcher.cpp" "/home/zn/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/AbstractFeatureMatcher.cpp.o"
+  "/home/zn/Chapter4_StructureFromMotion/BundleAdjuster.cpp" "/home/zn/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/BundleAdjuster.cpp.o"
+  "/home/zn/Chapter4_StructureFromMotion/Common.cpp" "/home/zn/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/Common.cpp.o"
+  "/home/zn/Chapter4_StructureFromMotion/Distance.cpp" "/home/zn/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/Distance.cpp.o"
+  "/home/zn/Chapter4_StructureFromMotion/FindCameraMatrices.cpp" "/home/zn/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/FindCameraMatrices.cpp.o"
+  "/home/zn/Chapter4_StructureFromMotion/GPUSURFFeatureMatcher.cpp" "/home/zn/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/GPUSURFFeatureMatcher.cpp.o"
+  "/home/zn/Chapter4_StructureFromMotion/MultiCameraDistance.cpp" "/home/zn/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/MultiCameraDistance.cpp.o"
+  "/home/zn/Chapter4_StructureFromMotion/MultiCameraPnP.cpp" "/home/zn/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/MultiCameraPnP.cpp.o"
+  "/home/zn/Chapter4_StructureFromMotion/OFFeatureMatcher.cpp" "/home/zn/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/OFFeatureMatcher.cpp.o"
+  "/home/zn/Chapter4_StructureFromMotion/RichFeatureMatcher.cpp" "/home/zn/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/RichFeatureMatcher.cpp.o"
+  "/home/zn/Chapter4_StructureFromMotion/SfMUpdateListener.cpp" "/home/zn/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/SfMUpdateListener.cpp.o"
+  "/home/zn/Chapter4_StructureFromMotion/Triangulation.cpp" "/home/zn/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/Triangulation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_OPENNI2"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  "USE_EIGEN"
+  "qh_QHpointer"
+  "vtkFiltersFlowPaths_AUTOINIT=1(vtkFiltersParallelFlowPaths)"
+  "vtkIOExodus_AUTOINIT=1(vtkIOParallelExodus)"
+  "vtkIOGeometry_AUTOINIT=1(vtkIOMPIParallel)"
+  "vtkIOImage_AUTOINIT=1(vtkIOMPIImage)"
+  "vtkIOSQL_AUTOINIT=2(vtkIOMySQL,vtkIOPostgreSQL)"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL)"
+  "vtkRenderingCore_AUTOINIT=4(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingFreeTypeOpenGL,vtkRenderingOpenGL)"
+  "vtkRenderingFreeType_AUTOINIT=2(vtkRenderingFreeTypeFontConfig,vtkRenderingMatplotlib)"
+  "vtkRenderingLIC_AUTOINIT=1(vtkRenderingParallelLIC)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/opencv-2.4.9/include"
+  "/usr/include/vtk-6.2"
+  "/usr/include/jsoncpp"
+  "/usr/include/hdf5/openmpi"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent/include"
+  "/usr/lib/openmpi/include"
+  "/usr/lib/openmpi/include/openmpi"
+  "/usr/include/libxml2"
+  "/usr/include/freetype2"
+  "/usr/include/x86_64-linux-gnu/freetype2"
+  "/usr/include/x86_64-linux-gnu"
+  "/usr/include/python2.7"
+  "/usr/include/tcl"
+  "/usr/include/eigen3"
+  "/usr/local/opencv-2.4.9/include/opencv"
+  "/usr/include/pcl-1.7"
+  "/usr/include/ni"
+  "/home/zn/SSBA-3.0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
